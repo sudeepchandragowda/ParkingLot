4766be52986e53b5f646d54599bbb64c0e1408ff
Sudeep Chandra B S <122792474+sudeepchandragowda@users.noreply.github.com>
2024-04-28 04:51:46 +0530

    Added models to ParkingLot

--- src/models/ParkingSpot.java
+++ src/models/ParkingSpot.java
@@ -5,4 +5,36 @@ public class ParkingSpot extends BaseModel {
     private VehicleType supportedVehicleType;
     private Status status;
     private Vehicle vehicle;
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public VehicleType getSupportedVehicleType() {
+        return supportedVehicleType;
+    }
+
+    public void setSupportedVehicleType(VehicleType supportedVehicleType) {
+        this.supportedVehicleType = supportedVehicleType;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public Vehicle getVehicle() {
+        return vehicle;
+    }
+
+    public void setVehicle(Vehicle vehicle) {
+        this.vehicle = vehicle;
+    }
 }

5424cfb557dc28c2e545b0fe5771cd6bb9915fd7
Sudeep Chandra B S <122792474+sudeepchandragowda@users.noreply.github.com>
2024-05-08 23:57:50 +0530

    Started creating initialisation service without compilation

---
+++ src/service/InitialisationService.java
@@ -0,0 +1,10 @@
+package service;
+
+import repository.GateRepository;
+
+public class InitialisationService {
+    private GateRepository gateRepository;
+    private ParkingFloorRepository parkingFloorRepository;
+    private ParkingSpotRepository parkingSpotRepository;
+    private ParkingLotRepository parkingLotRepository;
+}

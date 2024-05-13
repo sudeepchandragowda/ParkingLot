bc3cd27ac56979d69b34198552784ccfa5330b6d
Sudeep Chandra B S <122792474+sudeepchandragowda@users.noreply.github.com>
2024-05-14 00:00:03 +0530

    Daily commit

--- src/service/TicketService.java
+++ src/service/TicketService.java
@@ -7,6 +7,8 @@ import repository.TicketRepository;
 import strategy.spotAllocationStrategy.SpotAllocationFactory;
 import strategy.spotAllocationStrategy.SpotAllocationStrategy;
 
+import java.time.LocalDateTime;
+
 public class TicketService {
     private TicketRepository ticketRepository;
     private ParkingLotRepository parkingLotRepository;
@@ -18,13 +20,18 @@ public class TicketService {
         this.gateRepository = gateRepository;
     }
 
-    public Ticket getTicket(VehicleType vehicleType, String vehicleNumber, String vehicleColor, int gateId) {
+    public Ticket getTicket(VehicleType vehicleType, String vehicleNumber, String vehicleColor, String vehicleMake, int gateId) {
         Gate gate = gateRepository.get(gateId);
         ParkingLot parkingLot = parkingLotRepository.getParkingLotFromGate(gate);
         SpotAllocationStrategy spotAllocationStrategy = SpotAllocationFactory.getSpotAllocationStrategy(parkingLotRepository);
         ParkingSpot parkingSpot = spotAllocationStrategy.getSpot(vehicleType, gate);
 
-        Ticket ticket = new Ticket();
-        ticket.setVehicle(new Vehicle());
+        Vehicle v = new
+                Ticket ticket = new Ticket();
+        ticket.setVehicle(new Vehicle(vehicleNumber, vehicleColor, vehicleMake, vehicleType ));
+        ticket.setEntryTime(LocalDateTime.now());
+        ticket.setParkingSpot(parkingSpot);
+        ticket.setVehicle();
+
     }
 }
